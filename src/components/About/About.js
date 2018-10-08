ce77a2872f8622abfa9d6079e9fad99c22b1aa5c
Brevon Good <brevonbgood@comcast.net>
2018-10-08 14:54:30 -0600

    remove unnecessary Link imports and update result.css, about.css

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 import './about.css'
 
 class About extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Levels Beyond Notes React App</h1>
-        <p>This app calls the GitHub issues Api for the Angualar Repo.</p>
+      <div className='about container'>
+        <h1 className='about-header'>Levels Beyond Notes React App</h1>
+        <p className='about-desc'>This app calls the GitHub issues Api for the Angualar Repo.</p>
       </div>
     )
   }
--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import ReactMarkdown from 'react-markdown'
 import { Markdown } from 'react-showdown';
-import {Link} from 'react-router-dom'
 import './result.css'
 
 class Result extends React.Component {
@@ -20,18 +18,17 @@ class Result extends React.Component {
   }
 
   render() {
-    let markdown = "<style>h2 {color: red;}</style>" + this.props.issue.body
     return (
       <div className='issue'>
         <div className='issue-header'>
           <div>
             <p className='issue-title' onClick={this.toggleActive}>{this.props.issue.title}</p>
-            <p className='issue-subtitle'>#{this.props.issue.number} updated on {this.props.issue.updated_at} by <a className='username' href={this.props.issue.user.html_url} target='_blank'>{this.props.issue.user.login}</a></p>
+            <p className='issue-subtitle'>#{this.props.issue.number} updated on {this.props.issue.updated_at} by <a className='username' href={this.props.issue.user.html_url} target='_blank' rel="noopener noreferrer">{this.props.issue.user.login}</a></p>
             <a className='' href={this.props.issue.html_url}>Link to PR on GitHub</a>
           </div>
           { this.state.active ? <p className='x' onClick={this.toggleActive}>X</p> : null }
         </div>
-        { this.state.active ? <Markdown markdown={markdown} /> : null }
+        { this.state.active ? <Markdown markdown={this.props.issue.body} /> : null }
       </div>
     )
   }
